230db36f974de65eff49cbfe9a6cc96d72910d24
Nicolas Barbetta <nbarbetta@gmail.com>
2021-11-20 14:56:40 -0300

    final

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ from src.extra_b import *
 
 cadenaExtraB=input('ingrese su cadena para contar caractares: ')
 contarCaracteres(cadenaExtraB)
+
+#Ejercico 5 ...
+
+from src.punto_5 import *
+
+lista = [5,8,4,9,10,6,10]
+
+newlist = segundo(lista)
+print(newlist)
